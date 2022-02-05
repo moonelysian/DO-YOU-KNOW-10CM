6219df8b6cd52447eb9be517c27e8bf6e2fd4bfe
moonelysian <37898263+moonelysian@users.noreply.github.com>
2022-02-05 16:39:26 +0900

    fix: styled-components로 묶고 작은 pc화면도 대응

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -2,25 +2,46 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
 background: black;
-min-width: 100vw;
-min-height: 100vh;
+`
+const InnerWrapper = styled.div`
+max-width: 90%;
+margin: 0 auto;
+`
+
+const EmptyBlock = styled.div`
+width: 100%;
+height: 10vh;
+`
+const Block = styled.div`
+width: 100%;
+height: 40vh;
+text-align: center;
 
 #circle {
-  margin-right: -20px;
-  margin-bottom: -60px;
+  height: 50%;
+  margin-bottom: -1%;
+  margin-right: -5%;
+
 }
 #logo {
-  transform: rotate(17.74deg); 
-  margin-bottom: -120px;
+  height: 100%;
+  transform: rotate(21.74deg);
+  margin-top: 4%;
 }
 #un {
+  height: 60%;
   transform: rotate(18.74deg); 
+  margin-right: 10px;
 }
 #official {
+  height: 55%;
   transform: rotate(-6.95deg);
+  margin-right: -10%;
 }
 #arrow {
-  transform: rotate(-22.48deg)
+  height: 55%;
+  transform: rotate(-22.48deg);
+  cursor: pointer;
 }
 
 .shape-hover {
@@ -29,42 +50,47 @@ min-height: 100vh;
 }
 .shape-hover#circle {
   transition: 1.5s ease-in;
-  margin-bottom: -15px;
-  margin-right: 100px;
+  margin-bottom: 6%;
+  margin-right: 10px;
 }
 .shape-hover#un {
   transition: 1.5s ease-in;
-  margin: 0px 25px 0px;
+  margin-right: 20px;
+  transform: rotate(23deg) !important;
 }
 .shape-hover#official {
-    margin-right: -50px;
-    transform: rotate(-2deg) !important;
-    transition: 1.5s ease-in;
+  transition: 1.5s ease-in;
+  margin-right: -8%;
 }
 `
 
 const Home = ():JSX.Element => {
-  const changeClassName = () => {
-    const lst = document.getElementsByTagName('img');
-    const len = lst.length
+  const changeClassName = (addClass: string, removeClass?: string) => {
+    const doms = document.getElementsByTagName('img');
+    const len = doms.length
     for (let i = 0; i < len; i++) {
-      lst[i].classList.add('shape-hover')
-      lst[i].classList.remove('shapes')
+      doms[i].classList.add(addClass)
+      removeClass && doms[i].classList.remove(removeClass)
     }
   }
+
   return (
     <Wrapper>
-      <div>
-        <div style={{paddingTop: '15vh', paddingLeft: '40vw'}}>
+      <InnerWrapper>
+        <EmptyBlock/>
+        <Block>
           <img className='shapes' id='circle' src='/images/circle.png' alt='circle'/>
           <img className='shapes' id='logo' src='/images/logo-back.png' alt='logo'/>
-        </div>
-        <div style={{paddingLeft: '10vw'}}>
+        </Block>
+        <Block>
           <img className='shapes' id='un' src='/images/un.png' alt='unofficial'/>
           <img className='shapes' id='official' src='/images/official.png' alt='unofficial'/>
-          <img className='shapes' id='arrow' src='/images/arrow.png' alt='unofficial' onMouseOver={() => changeClassName()}/>
-        </div>
-      </div>
+          <img className='shapes' id='arrow' src='/images/arrow.png' alt='arrow' 
+            onMouseOver={() => changeClassName('shape-hover')}
+          />
+        </Block>
+        <EmptyBlock/>
+      </InnerWrapper>
     </Wrapper>
   );
 }
